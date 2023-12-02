036b3f98a68dd414759b864a2a55a722da7a2216
Robert <turbobert-github@mailbox.org>
2023-12-02 10:43:58 +0100

    ...

---
+++ repos/qrl/src/startup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+. /py/bin/activate
+
+export PYTHONPATH=/qrl/src
+
+
+#we're already in /work whcih is PWD outside
+
+set -x
+
+if [[ -f text ]]; then
+    cat text | python -m qrl text -
+fi
