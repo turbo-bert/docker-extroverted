c718628598efe05736c1f1e5118f966e22de4ea0
Robert <turbobert-github@mailbox.org>
2023-12-02 09:53:19 +0100

    ...

---
+++ repos/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+TARGETDIR=${1:-~/bin}
+
+echo -en "If you want to install wrappers in $TARGETDIR, press ENTER to continue or CTRL-C to abort"
+read
+
+set -x
+for A in costa lit mutter poc; do
+    cp -f $A/$A $TARGETDIR/$A
+done
