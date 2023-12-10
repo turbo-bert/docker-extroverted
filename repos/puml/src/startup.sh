21acb2cab452c871324dc3beb4ce423537562f0c
Robert <turbobert-github@mailbox.org>
2023-12-10 21:12:11 +0100

    ...

--- repos/puml/src/startup.sh
+++ repos/puml/src/startup.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
 
-echo "plat uml wrapper"
+for f in "$(ls *.uml)"; do
+    ff=${f%%.uml}
+    set -x
+    (
+        echo "@startuml"
+        echo "skinparam monochrome true"
+        echo "skinparam dpi 150"
+        cat "$f"
+        echo "@enduml"
+    ) | plantuml -progress -tpng -p >$ff.png
+
+    (
+        echo "@startuml"
+        echo "skinparam monochrome true"
+        echo "skinparam dpi 150"
+        cat "$f"
+        echo "@enduml"
+    ) | plantuml -progress -tsvg -p >$ff.svg
+
+done
--- repos/puml/test/main.scad
+++
@@ -1,7 +0,0 @@
-include <parts/all.scad>;
-
-
-module mod_00000000000000000000000000000000() {
-    //gear(m=1.5, t=10, h=10);
-    elf_brick();
-}
