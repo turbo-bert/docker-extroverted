ff50283dc727ff1385142cdfe43696f41d871f34
Robert <turbobert-github@mailbox.org>
2023-12-10 21:23:09 +0100

    ...

--- repos/puml/src/startup.sh
+++ repos/puml/src/startup.sh
@@ -20,4 +20,28 @@ for f in "$(ls *.uml)"; do
         echo "@enduml"
     ) | plantuml -progress -tsvg -p >$ff.svg
 
+    (
+        echo "@startuml"
+        echo "skinparam monochrome true"
+        echo "skinparam dpi 150"
+        cat "$f"
+        echo "@enduml"
+    ) | plantuml -progress -tpdf -p >$ff.pdf
+
+    (
+        echo "@startuml"
+        echo "skinparam monochrome true"
+        echo "skinparam dpi 150"
+        cat "$f"
+        echo "@enduml"
+    ) | plantuml -progress -ttxt -p >$ff.txt
+
+    (
+        echo "@startuml"
+        echo "skinparam monochrome true"
+        echo "skinparam dpi 150"
+        cat "$f"
+        echo "@enduml"
+    ) | plantuml -progress -tutxt -p >$ff.utxt
+
 done
