bfa306b9d7aa5bd25f603364fcf9e1b3fd2803b5
Robert <turbobert-github@mailbox.org>
2023-12-15 07:59:24 +0100

    ...

--- repos/install_starters.sh
+++ repos/install_starters.sh
@@ -11,7 +11,7 @@ echo -en "If you want to install wrappers in $TARGETDIR, press ENTER to continue
 read
 
 set -x
-for A in costa costa74 lit mutter poc qrl dro parts2 puml dcx bk; do
+for A in costa costa74 lit mutter poc qrl dro parts2 puml dcx bk frp; do
     echo "=========================================="
     cp -f $A/$A $TARGETDIR/$A # short
     cp -f $A/$A $TARGETDIR/turbobert-$A # with turbobert-prefix
