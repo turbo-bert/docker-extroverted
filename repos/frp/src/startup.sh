f575b792b1013dd7b36675b084ef69a2fbeadbc5
Robert <turbobert-github@mailbox.org>
2023-12-15 07:59:29 +0100

    ...

---
+++ repos/frp/src/startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+#. /venv/bin/activate
+
+#python -m PYMOD -u
+#python -m PYMOD
+
+fpc main.pas
+
+#echo "startup.sh not customized yet!"
+#read
+#cd /app && python -u -m YOUR_MODULE
