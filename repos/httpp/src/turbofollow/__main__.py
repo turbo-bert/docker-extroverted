d0b925c2cd91b134e4171cb30f22b49fa7b98fed
Robert <turbobert-github@mailbox.org>
2024-07-10 20:34:06 +0200

    ...

--- repos/httpp/src/turbofollow/__main__.py
+++ repos/httpp/src/turbofollow/__main__.py
@@ -8,11 +8,28 @@ with open("/work/httpp.hosts", "w") as empty_the_file:
 
 p = subprocess.Popen(['/usr/bin/tinyproxy','-d'], stdout=subprocess.PIPE)
 
-for line in p.stdout:
-    u_line = line.decode()
-    print("%d %s" % (line_number, u_line))
 
-    with open("/work/httpp.hosts", "a") as f:
-        f.write("%s\n" % u_line)
+target_info = {}
+
+for line in p.stdout:
+    u_line = line.decode().strip()
+    if not u_line.startswith("INFO"):
+        continue
+    if u_line.find("opensock") < 0:
+        continue
+    if u_line.find("opening connection to") < 0:
+        continue
+    cols = u_line.split(" ")
+    target = cols[-1]
+
+    if target in target_info.keys():
+        target_info[target]+=1
+    else:
+        target_info[target]=1
+        with open("/work/httpp.hosts", "a") as f:
+            f.write("%s\n" % target)
+
+    print("%s - %d" % (target, target_info[target]))
+
     
     line_number+=1
